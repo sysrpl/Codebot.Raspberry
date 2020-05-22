b4a82ff203ed0207dca4a367fbc79055e94552f8
Anthony Walter <sysrpl@gmail.com>
2020-05-21 22:33:43 -0400

    Added Ws28xx example

--- Raspberry.Device/Ws28xx/src/Ws28xx.cs
+++ Raspberry.Device/Ws28xx/src/Ws28xx.cs
@@ -36,4 +36,45 @@ namespace Raspberry.Device
         /// </summary>
         public void Update() => _spiDevice.Write(Image.Data);
     }
+        /*static void TestWs28xx()
+        {
+            Console.WriteLine("Press return to start spi");
+            Console.ReadLine();
+            const int Count = 8;
+
+            var settings = new SpiConnectionSettings(0, 0)
+            {
+                ClockFrequency = 2_400_000,
+                Mode = SpiMode.Mode0,
+                DataBitLength = 8
+            };
+
+            // Create a Neo Pixel x8 stick on spi 0.0
+            var spi = SpiDevice.Create(settings);
+
+            var neo = new Raspberry.Device.Ws2812b(spi, Count);
+            BitmapImage img = neo.Image;
+            img.Clear();
+            img.SetPixel(0, 0, Color.White);
+            img.SetPixel(1, 0, Color.Red);
+            img.SetPixel(2, 0, Color.Green);
+            img.SetPixel(3, 0, Color.Blue);
+            img.SetPixel(4, 0, Color.Yellow);
+            img.SetPixel(5, 0, Color.Cyan);
+            img.SetPixel(6, 0, Color.Magenta);
+            img.SetPixel(7, 0, Color.FromArgb(unchecked((int)0xffff8000)));
+            neo.Update();
+            Console.WriteLine("Press return to exit");
+            Console.ReadLine();
+            img.Clear();
+            img.SetPixel(0, 0, Color.Black);
+            img.SetPixel(1, 0, Color.Black);
+            img.SetPixel(2, 0, Color.Black);
+            img.SetPixel(3, 0, Color.Black);
+            img.SetPixel(4, 0, Color.Black);
+            img.SetPixel(5, 0, Color.Black);
+            img.SetPixel(6, 0, Color.Black);
+            img.SetPixel(7, 0, Color.Black);
+            neo.Update();
+        }*/
 }
