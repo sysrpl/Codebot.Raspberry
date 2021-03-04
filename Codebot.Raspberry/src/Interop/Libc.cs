650de0e1cf2f26b04b4e3036792296abc8040e48
Anthony Walter <sysrpl@gmail.com>
2021-03-04 15:39:50 -0500

    Move to Interop

---
+++ Codebot.Raspberry/src/Interop/Libc.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Runtime.InteropServices;
+#pragma warning disable IDE1006 // Naming Styles
+
+namespace Codebot.Raspberry
+{
+    public static class Libc
+    {
+        const string libc = "libc.so.6";
+
+        public const int O_RDWR = 0x002;
+        public const int O_NOCTTY = 0x100;
+        public const uint TCFLSH = 0x540b;
+        public const int TCIFLUSH = 0;
+        public const int TCIOFLUSH = 2;
+        public const int TCOFLUSH = 1;
+        public const int TCSANOW = 0;
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "fopen64")]
+        public static extern IntPtr fopen(string filename, string mode);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "fclose")]
+        public static extern int fclose(IntPtr file);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "fread")]
+        public static extern IntPtr fread(byte[] data, IntPtr size, IntPtr nmemb, IntPtr file);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "fwrite")]
+        public static extern IntPtr fwrite(byte[] data, IntPtr size, IntPtr number, IntPtr file);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "fputs")]
+        public static extern int fputs(string str, IntPtr file);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "fgets")]
+        public static extern IntPtr fgets(string str, int num, IntPtr file);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "fprintf")]
+        public static extern int fprintf(IntPtr file, string format);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "fflush")]
+        public static extern int fflush(IntPtr file);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "ftruncate64")]
+        public static extern int ftruncate(int fd, long length);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "fseeko64")]
+        public static extern int fseek(IntPtr file, long offset, int whence);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "ftello64")]
+        public static extern long ftell(IntPtr file);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "ferror")]
+        public static extern int ferror(IntPtr file);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "feof")]
+        public static extern int feof(IntPtr file);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "fileno")]
+        public static extern int fileno(IntPtr file);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "clearerr")]
+        public static extern void clearerr(IntPtr file);
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct TermiosStruct
+        {
+            [MarshalAs(UnmanagedType.U4)]
+            [FieldOffset(0)]
+            public uint c_iflag;
+
+            [MarshalAs(UnmanagedType.U4)]
+            [FieldOffset(4)]
+            public uint c_oflag;
+
+            [MarshalAs(UnmanagedType.U4)]
+            [FieldOffset(8)]
+            public uint c_cflag;
+
+            [MarshalAs(UnmanagedType.U4)]
+            [FieldOffset(12)]
+            public uint c_lflag;
+
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 36)]
+            [FieldOffset(16)]
+            public byte[] c_cc;
+
+            [MarshalAs(UnmanagedType.U4)]
+            [FieldOffset(52)]
+            public uint c_ispeed;
+
+            [MarshalAs(UnmanagedType.U4)]
+            [FieldOffset(56)]
+            public uint c_ospeed;
+        }
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "open64")]
+        public static extern int open(string path, int flags);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "close")]
+        static public extern int close(int fd);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "write")]
+        public static extern int write(int fd, byte[] buffer, int numBytes);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "read")]
+        public static extern int read(int fd, byte[] buffer, int numBytes);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "ioctl")]
+        public static extern int ioctl(int fd, uint request, int value);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tcgetattr")]
+        public static extern int tcgetattr(int fd, ref TermiosStruct term);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "tcsetattr")]
+        public static extern int tcsetattr(int fd, int actions, ref TermiosStruct term);
+
+        public const int CLOCK_REALTIME = 0;
+        public const int CLOCK_MONOTONIC = 1;
+        public const int CLOCK_PROCESS_CPUTIME_ID = 2;
+        public const int CLOCK_THREAD_CPUTIME_ID = 3;
+        public const int CLOCK_MONOTONIC_RAW = 4;
+
+        public struct timespec
+        {
+            public IntPtr tv_sec;
+            public IntPtr tv_nsec;
+
+            public static timespec FromMilliseconds(double milliseconds)
+            {
+                if (milliseconds < 0)
+                    return new timespec();
+                uint s = (uint)(milliseconds / 1000);
+                long n = (long)(milliseconds - s) * 1_000_000_000;
+                return new timespec()
+                {
+                    tv_sec = (IntPtr)s,
+                    tv_nsec = (IntPtr)n
+                };
+            }
+
+            public double ToMilliseconds()
+            {
+                return (long)tv_sec * 1000d + (long)tv_nsec / 1_000_000d;
+            }
+        }
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "nanosleep")]
+        public static extern int nanosleep(ref timespec request, IntPtr nullptr);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "clock_gettime")]
+        public static extern int clock_gettime(int clockid, out timespec tp);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "clock_getres")]
+        public static extern int clock_getres(int clockid, out timespec res);
+
+        [DllImport(libc, CallingConvention = CallingConvention.Cdecl, EntryPoint = "clock_nanosleep")]
+        public static extern int clock_nanosleep(int clockid, int flags, ref timespec request, IntPtr nullptr);
+    }
+}
---
+++ Codebot.Raspberry/src/Interop/UnixFile.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.IO;
+using static Codebot.Raspberry.Libc;
+
+/* The following modes can be used with UnixFile
+
+   Mode   Description
+   ------ ---------------------------------------------------------   
+    r      read (file must exist). starts at beginning
+    w      write (deletes context or creates file if it doesn't exist). starts at beginning
+    a      write appending (creates file if it doesn't exist). starts at end
+    r+     read write (file must exist). starts at beginning
+    w+     read write (deletes context or creates file if it doesn't exist). starts at beginning
+    a+     read write appending (creates file if it doesn't exist). starts at beginning  */
+
+namespace Codebot.Raspberry
+{
+    public enum UnixSeek
+    {
+        // Seek from the beginning of the stream
+        Begining = 0,
+        // Seek from the current position of the stream
+        Current = 1,
+        // Seek backwards from the end of the stream
+        End = 2
+    }
+
+    /// <summary>
+    /// A file using purely Unix functions.
+    /// </summary>
+    public class UnixFile : IDisposable
+    {
+        IntPtr file;
+
+        void CheckDisposed()
+        {
+            if (file == IntPtr.Zero)
+                throw new ObjectDisposedException(GetType().ToString());
+        }
+
+        /// <summary>
+        /// Create or open a file given a filename and optional mode.
+        /// </summary>
+        public UnixFile(string fileName, string mode = "a+")
+        {
+            FileName = fileName;
+            file = fopen(fileName, mode);
+            if (file == IntPtr.Zero)
+                throw new IOException($"Could not open {fileName}.");
+        }
+
+        /// <summary>
+        /// Reopen the current file using a different mode.
+        /// </summary>
+        public void Reopen(string mode)
+        {
+            Dispose();
+            file = fopen(FileName, mode);
+            if (file == IntPtr.Zero)
+                throw new IOException($"Could not open {FileName}.");
+        }
+
+        /// <summary>
+        /// Open a different file.
+        /// </summary>
+        public void Change(string fileName, string mode = "a+")
+        {
+            Dispose();
+            FileName = fileName;
+            file = fopen(fileName, mode);
+            if (file == IntPtr.Zero)
+                throw new IOException($"Could not open {fileName}.");
+        }
+
+        /// <summary>
+        /// EOF is true if the position indicator is and the end of the file.
+        /// </summary>
+        public bool EOF
+        {
+            get
+            {
+                return Position == Length;
+            }
+        }
+
+        /// <summary>
+        /// The first error number encountered if any are set.
+        /// </summary>
+        public int Error
+        {
+            get
+            {
+                CheckDisposed();
+                return ferror(file);
+            }
+        }
+
+        /// <summary>
+        /// The name of the file associated with the unix file object.
+        /// </summary>
+        public string FileName { get; private set; }
+
+        /// <summary>
+        /// Get the length of the file in bytes.
+        /// </summary>
+        public long Length
+        {
+            get
+            {
+                CheckDisposed();
+                var p = ftell(file);
+                fseek(file, 0, (int)UnixSeek.End);
+                var length = ftell(file);
+                fseek(file, p, (int)UnixSeek.Begining);
+                return length;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the position indicator associated with the unix file.
+        /// </summary>
+        /// <value>The position.</value>
+        public long Position
+        {
+            get
+            {
+                CheckDisposed();
+                return ftell(file);
+            }
+            set
+            {
+                Seek(value);
+            }
+        }
+
+        /// <summary>
+        /// Clears any errors associated with the file.
+        /// </summary>
+        public void ClearError()
+        {
+            CheckDisposed();
+            clearerr(file);
+        }
+
+        /// <summary>
+        /// Read from a file into an array of bytes.
+        /// </summary>
+        public int Read(byte[] buffer)
+        {
+            CheckDisposed();
+            return (int)fread(buffer, (IntPtr)1, (IntPtr)buffer.Length, file);
+        }
+
+        /// <summary>
+        /// Read from a file length bytes and convert it to a string.
+        /// </summary>
+        public string Read(int length)
+        {
+            var buffer = new byte[length];
+            var count = Read(buffer);
+            return System.Text.Encoding.UTF8.GetString(buffer, 0, count);
+        }
+
+        /// <summary>
+        /// Write an array of bytes to the file.
+        /// </summary>
+        public int Write(byte[] buffer)
+        {
+            CheckDisposed();
+            var written = (int)fwrite(buffer, (IntPtr)1, (IntPtr)buffer.Length, file);
+            fflush(file);
+            return written;
+        }
+
+        /// <summary>
+        /// Write a string to the file.
+        /// </summary>
+        public int Write(string text)
+        {
+            var buffer = System.Text.Encoding.UTF8.GetBytes(text);
+            return Write(buffer);
+        }
+
+        /// <summary>
+        /// Reset the file to have a length of zero.
+        /// </summary>
+        public void Reset()
+        {
+            CheckDisposed();
+            ftruncate(fileno(file), 0);
+            fflush(file);
+        }
+
+        /// <summary>
+        /// Seek to a point in the file.
+        /// </summary>
+        public void Seek(long position, UnixSeek whence = UnixSeek.Begining)
+        {
+            CheckDisposed();
+            fseek(file, position, (int)whence);
+        }
+
+        /// <summary>
+        /// Truncate the file at its current position.
+        /// </summary>
+        public void Truncate()
+        {
+            CheckDisposed();
+            ftruncate(fileno(file), ftell(file));
+            fflush(file);
+        }
+
+        /// <summary>
+        /// Dispose releases all resources used by this object.
+        /// </summary>
+        public void Dispose()
+        {
+            if (file == IntPtr.Zero)
+                return;
+            fclose(file);
+            file = IntPtr.Zero;
+        }
+    }
+}
