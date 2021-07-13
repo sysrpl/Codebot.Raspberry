ea360f512c5a8cb3dbe4307cc9b979a15aa15d1e
Anthony Walter <sysrpl@gmail.com>
2021-07-13 13:00:43 -0400

    Added Program.cs

---
+++ Tests/src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tests
+{
+
+    class MainClass
+    {
+        // GPIO access requires these packages: gpiod libgpiod-dev libgpiod-doc
+        public static void Main(string[] args)
+        {
+            PulseTest.Run();
+        }
+    }
+}
